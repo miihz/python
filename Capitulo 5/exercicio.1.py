c167e7fa4cdc710323d3b6a7b622cdc595477096
miihz <santos.lima.emilly@escola.pr.gov.br>
2025-09-24 11:03:48 -0300

    commit

---
+++ Capitulo 5/exercicio.1.py
@@ -0,0 +1,8 @@
+def soma():
+    numero1 = int(input("digite o primeiro numero"))
+    numero2 = int(input("digite o segundo numero"))
+
+    total = numero1 + numero2
+    print(f"{numero1} + {numero2} = {total}")
+
+soma()
---
+++ Capitulo 5/exercicio.2.py
@@ -0,0 +1,11 @@
+def resultadoFinal(numSomados,valentia):
+    return imprimir
+num1 = int(input("digite um numero"))
+num2 = int(input("digite outro numero"))
+num3 = int(input("digite mais um numero"))
+num4 = int(input("digite o último numero"))
+
+numSomados = (num1 + num2 + num3 + num4)
+valentia = 10
+
+resultadoFinal(numSomados,valentia)
